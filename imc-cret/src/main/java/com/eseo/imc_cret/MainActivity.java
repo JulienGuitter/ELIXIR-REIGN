8917f1974b5649ba686cbc0e8306f09d319a0e1e
Julien Guitter <julien.guitter@outlook.com>
2026-04-02 09:44:30 +0200

    Feat: implement cours app part laucher and link the game to it

---
+++ imc-cret/src/main/java/com/eseo/imc_cret/MainActivity.java
@@ -0,0 +1,46 @@
+package com.eseo.imc_cret;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String CORE_PACKAGE = "com.mjm.elixir_reign";
+    private static final String CORE_ACTIVITY = "com.mjm.elixir_reign.android.AndroidLauncher";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button openCoreButton = findViewById(R.id.open_core_button);
+        openCoreButton.setOnClickListener(v -> openCore());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void openCore() {
+        Intent intent = new Intent();
+        intent.setClassName(CORE_PACKAGE, CORE_ACTIVITY);
+
+        try {
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, R.string.core_not_installed, Toast.LENGTH_LONG).show();
+        }
+    }
+}
